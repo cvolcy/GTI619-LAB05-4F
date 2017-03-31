a7fb9cf04c972237c64fa21b4aae2613fc9a866a
Samuel Laroche <samuel@jfzone.net>
2017-03-31 17:02:05 -0400

    add securtiy view
    populate form

--- app/routes/security.js
+++ app/routes/security.js
@@ -4,11 +4,17 @@ const express = require('express'),
 let router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render('security');
+  let Setting = mongoose.model("SecuritySettings");
+
+  Setting.findOne({}).then((result) => {
+    res.render('security', { passwordRules: result.passwordRules, passwordChange: result.passwordChange, bruteforce: result.bruteforce });
+  }).catch((err) => {
+    res.render('test', { result: JSON.stringify(err) });
+  });
 });
 
 router.post("/setting", (req, res, next) => {
-  res.send('/secutity [POST]');
+  res.render('test', { result: JSON.stringify(req.body, null, 2)});
 });
 
 module.exports = router;
