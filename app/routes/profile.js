87a0c129f72d709c6a5aed071409c264d42cdcac
Samuel Laroche <samuel@jfzone.net>
2017-04-06 15:05:52 -0400

    fix change password

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,72 +1,69 @@
 /**
  * Created by jeanym on 2017-04-01.
  */
-const express = require('express');
+const express = require('express'),
+      mongoose = require('mongoose');
 
 module.exports = function(app) {
-    let router = express.Router();
+  let router = express.Router();
 
-    // =====================================
-    // PROFILE SECTION =====================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
-    router.get('/', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
-            user : req.user // get the user out of session and pass to template
+  // =====================================
+  // PROFILE SECTION =====================
+  // =====================================
+  // we will want this protected so you have to be logged in to visit
+  // we will use route middleware to verify this (the isLoggedIn function)
+  router.get('/', isLoggedIn, function(req, res) {
+      res.render('profile.ejs', {
+          user : req.user // get the user out of session and pass to template
+      });
+  });
+
+  router.post('/password', isLoggedIn, function(req, res) {
+    let bcrypt = require('bcrypt');
+    let User = mongoose.model("User");
+    var uPass = req.user.password;
+    var oldPassword = req.body.password;
+    var newPassword = req.body.password_new;
+    var valPassword = req.body.password_val;
+    let isPasswordValid = bcrypt.compareSync(oldPassword, uPass);
+
+    User.findById(req.user.id).populate('passwordHistory').then((user) => {
+      if (!isPasswordValid) {
+        var msg = 'Incorrect password.';
+      } else if (valPassword !== newPassword) {
+        var msg = 'The new password and validation password doesnt match.';
+      } else {
+        let passExist = false;
+        user.passwordHistory.every(function(value){
+          if(bcrypt.compareSync(newPassword, value.password)) {
+            passExist = true;
+            return false;
+          } else {
+            return true;
+          }
         });
-    });
-    router.post('/password', isLoggedIn, function(req, res) {
+        if(!passExist && oldPassword != newPassword) {
+          user.password = user.hashPassword(newPassword);
+          user.save().then((user) => {
+            console.log(user.id);
+          }).catch((err) => {
+            console.log(err);
+          });
+          var msg = 'Success! Your password has been changed.';
+        } else {
+          var msg = 'Your new password must not be the same as one of your old passwords..';
+        }
+        console.log(msg);
+        res.redirect('/profile');
+      }
+    })
+  });
 
-            var uPass = req.user.password;
-            var oldPassword = req.body.password;
-            var newPassword = req.body.password_new;
-            var valPassword = req.body.password_val;
-            var bcrypt = require('bcrypt');
+  router.post('/email', isLoggedIn, function(req, res) {
+    res.send("email");
+  });
 
-            if (!bcrypt.compareSync(oldPassword, uPass)) {
-                var message = {
-                    message: 'Incorrect password.'
-                };
-            }else if (valPassword !== newPassword){
-                var message = {
-                    message: 'The new password and validation password doesnt match.'
-                };
-
-            }else{
-                var countSame = 0;
-                req.user.passwordHistory.forEach(function(value){
-                    if(bcrypt.compareSync(oldPassword, value)  || bcrypt.compareSync(value, uPass)) {
-                        countSame += 1;
-                    }
-                });
-                if(countSame == 0){
-                    req.user.password = newPassword;
-                    /*                            user.save(function(err) {
-                     req.logIn(user, function(err) {
-                     done(err, user);
-                     });
-                     });*/
-
-                    var message = {
-                        message: 'Success! Your password has been changed.'
-                    };
-
-                }else{
-
-                    var message = {
-                        message: 'Your new password must not be the same as one of your old passwords..'
-                    };
-                }
-                res.send( message);
-            }
-        });
-
-    router.post('/email', isLoggedIn, function(req, res) {
-        res.send("email");
-    });
-
-    return router;
+  return router;
 }
 
 // route middleware to make sure a user is logged in
